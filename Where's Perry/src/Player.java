b6a91655ce53637645ac759c923a7dc0f821b255
anagha166 <anaghakamath@gmail.com>
2023-05-12 11:40:30 -0700

    Classes added

--- Where's Perry/src/Player.java
+++ Where's Perry/src/Player.java
@@ -1,4 +1,53 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
-public class Player {
+public class Player{
+	private int x,y;
+ 	private int vx,vy;
+	private Image img; 	
+	private AffineTransform tx;
+
+	public Player() {
+		img = getImage("/imgs/phineas.png"); //load the image for Tree
+		
+
+		tx = AffineTransform.getTranslateInstance(0, 0);
+		init(0, 0); 				//initialize the location of the image
+									//use your variables
+	}
+	
+	public void changePicture(String newFileName) {
+		img = getImage(newFileName);
+		init(0, 0);
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, tx, null);
+
+	}
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(.5, .5);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Player.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
 
 }
