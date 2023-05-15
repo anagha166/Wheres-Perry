d54fa7ae54a5a63ca1db7dac6fa8a18ddcf67c53
palinakarzhenka <palinakarzhenka@10.9.130.254>
2023-05-15 11:55:28 -0700

    homepage(frame and new buttons) as well as levels

--- Where's Perry/src/Frame.java
+++ Where's Perry/src/Frame.java
@@ -14,12 +14,56 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import imgs.Background;
+import imgs.Level1;
+import imgs.Level2;
+import imgs.Level3;
+import imgs.LevelBackground;
+import imgs.Buttons;
+import imgs.RestartMenu;
+
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
-	Player p = new Player();
+	
+	//Player p = new Player();
+	Background homepage = new Background();
+	Buttons one = new Buttons("level1logo.png", 130, 190);
+	Buttons two = new Buttons("level2logo.png", 230, 190);
+	Buttons three = new Buttons("level3logo.png", 520, 190);
+	Buttons pause = new Buttons("pause.png", 730, 0);
+	
+	LevelBackground back = new LevelBackground();
+	Level1 first = new Level1();
+	boolean firstStart = false;
+	Level2 second = new Level2();
+	boolean secondStart = false;
+	Level3 third = new Level3();
+	boolean thirdStart = false;
+	
+	RestartMenu restartMenu = new RestartMenu();
 	
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		p.paint(g);
+		//p.paint(g);
+		homepage.paint(g);
+		one.paint(g);
+		two.paint(g);
+		three.paint(g);
+		
+		if (firstStart) {
+			back.paint(g);
+			first.paint(g);
+			pause.paint(g);
+		}
+		if (secondStart) {
+			back.paint(g);
+			second.paint(g);
+			pause.paint(g);
+		}
+		if (thirdStart) {
+			back.paint(g);
+			third.paint(g);
+			pause.paint(g);
+		}
 	}
 	
 	
@@ -29,8 +73,8 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	
 	public Frame() {
 		JFrame f = new JFrame("Wheres Perry");
-		f.setSize(new Dimension(1000, 1000));
-		f.setBackground(Color.blue);
+		f.setSize(new Dimension(800, 600));
+		f.setBackground(Color.white);
 		f.add(this);
 		f.setResizable(false);
 		f.setLayout(new GridLayout(1,2));
@@ -63,8 +107,17 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-	
+
+		if(one.hit(arg0)) {
+			firstStart = true;
+		}
+		if(two.hit(arg0)) {
+			secondStart = true;
+		}
+		if(three.hit(arg0)) {
+			thirdStart = true;
+		}
+		
 	}
 
 	@Override
